2fc6e2c69e21640f8f35d7da524e0db1e3bf215e
aokisinn <aokisinn1826@gmail.com>
2021-11-18 05:55:43 +0900

    補足コメント追加

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -3,13 +3,15 @@ import { useState } from "react"
 /**
  * データを事前に決める必要がある場合はこちらに処理を記述
  * サーバー側で描画をする
+ * 事前に決めるわけではなく動的に決める必要がある場合はこちらではなく
+ * メインの処理に記載する
  */
 export const getServerSideProps = () => {
     // ここで初期値が色々設定可能
     return { props: { initialCount: 10 } }
 }
 
-export default function Home({ initialCount }) { // initiaCountがgeterverSidePropsから受け取れる
+export default function Counter({ initialCount }) { // initiaCountがgeterverSidePropsから受け取れる
     const [count, setCount] = useState(initialCount)
     return (
         <div>
