00a8252152fa160a779ac63aeab378b99b645e65
aokisinn <aokisinn1826@gmail.com>
2021-11-18 05:53:43 +0900

    コメント追加

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
 
+/**
+ * データを事前に決める必要がある場合はこちらに処理を記述
+ * サーバー側で描画をする
+ */
 export const getServerSideProps = () => {
     // ここで初期値が色々設定可能
     return { props: { initialCount: 10 } }
