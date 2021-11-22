d73f06f1f74ec844684f75d066c6f8f30240ea07
aokisinn <aokisinn1826@gmail.com>
2021-11-22 19:59:29 +0900

    静的サイト追加

---
+++ pages/static/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>static site</title>
+            </Head>
+            <h1>This is static Site!</h1>
+            <div>Hello !</div>
+            <Link href="./about">about</Link>
+        </div>
+    )
+}
